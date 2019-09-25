57f0fb1187aebc2f888ea344a21e300c8b8c547d
hoandreas <andreas_ho@hotmail.com>
2019-09-25 12:57:48 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,6 @@ router.get('/user/login', (req,res) => res.render('login_form'));
 
 router.get('/user/register', (req,res) => res.render('registration_form'));
 
+router.get('/checkout', (req, res) => res.render('invoice'));
+
 module.exports =  router;
