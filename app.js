f2e7a17456af8c0b2ece7bb17fa40783bdd33f7b
hoandreas <andreas_ho@hotmail.com>
2019-09-26 15:24:19 +0700

    adding middleware and session

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const routes = require('./routes');
+const session = require('express-session');
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }))
 app.use('/', routes);
+
 
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
